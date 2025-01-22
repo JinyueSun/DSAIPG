3e8a88928a90ecb6982fbb1d106dfdf1fd939971
JinyueSun <sun.jiny@northeastern.edu>
2025-01-21 23:57:24 -0500

    config enviroment

--- src/main/java/com/phasmidsoftware/dsaipg/misc/NewtonApproximation.java
+++ src/main/java/com/phasmidsoftware/dsaipg/misc/NewtonApproximation.java
@@ -6,16 +6,19 @@ package com.phasmidsoftware.dsaipg.misc;
 
 class NewtonApproximation {
     public static void main(String[] args) {
-        // Newton's Approximation to solve cos(x) = x
+        // Newton's Approximation to solve sin(x) = x
         double x = 1.0;
         int left = 200;
         for (; left > 0; left--) {
-            final double y = Math.cos(x) - x;
+            // calculate f(x)
+            final double y = Math.sin(x) - x;
+            // check if f(x) < tolerance
             if (Math.abs(y) < 1E-7) {
-                System.out.println("the solution to cos(x)=x is: " + x);
+                System.out.println("the solution to sin(x)=x is: " + x);
                 System.exit(0);
             }
-            x = x + y / (Math.sin(x) + 1);
+            // update x = x - f(x) / f'(x)
+            x = x - y / (Math.cos(x) - 1);
         }
     }
 }
